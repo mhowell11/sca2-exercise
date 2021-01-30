7f73d8a60a7b7e606106224fa3fad7cbcde65c23
mhowell11 <meganghowell@gmail.com>
2021-01-30 13:48:31 -0700

    Day 1 Coding

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -1,47 +1,78 @@
-const assert = require("assert")
+const assert = require("assert");
 
 // Feel free to look things up online!
 
 const reverse = function(str) {
-  // TODO - write a function which reverses the string
+    // TODO - write a function which reverses the string
+    let splitString = string1.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return console.log(joinArray);
 }
 
-const factorial = function(num) {
-  // TODO - write a function which returns the factorial of a positive integer
-  // For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
+reverse("Hello World!");
+
+
+let factorial = function(num) {
+    // TODO - write a function which returns the factorial of a positive integer
+    // For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
+    let ans = 1;
+    while (num > 1) {
+        ans = ans * num;
+        num -= 1;
+    }
+    factorial = ans;
+    return console.log(factorial);
 }
+
+factorial(5);
+
 
 const announceDate = function() {
-  // TODO - write a function which returns the current date in the following format:
-  // "Today's date is January 7th, 2016. It is eleven thirty-seven in the morning."
+    // TODO - write a function which returns the current date in the following format:
+    // "Today's date is January 7th, 2016. It is eleven thirty-seven in the morning."
+    const date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let time = date.getTime();
+    console.log(`Today\'s date is ${month} ${day} ${year}. It is ${time}.`);
 }
+
+announceDate();
 
 const shiftRight = function(str, num) {
-  // TODO - write a function which shifts a string `num` characters to the right
-  // Example = shiftThree("Hello") === "lloHe"
-}
+        // TODO - write a function which shifts a string `num` characters to the right
+        // Example = shiftThree("Hello") === "lloHe"}
+        let letters = str.split("");
 
-const tokenize = function(str) {
-  // TODO - write a function which converts a multi-word string into an array of words
-}
+        for (let i = 0; i < num; i++) {
+            //letters.unshiftletters.pop();
+        }
+    }
+    /*
+           const tokenize = function(str) {
+               // TODO - write a function which converts a multi-word string into an array of words
+           }
 
-const uniqueOnes = function(ary) {
-  // TODO - write a function which returns the inputted array without duplicate elements
-}
+           const uniqueOnes = function(ary) {
+               // TODO - write a function which returns the inputted array without duplicate elements
+           }
 
-const zip = function(arr1, arr2) {
-  // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
-  // if the two arrays are of unequal length
-  // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
-}
+           const zip = function(arr1, arr2) {
+               // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
+               // if the two arrays are of unequal length
+               // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
+           }
 
-const unzip = function(arr) {
-  // TODO - Write a function which does the opposite of `zip()`
-}
+           const unzip = function(arr) {
+               // TODO - Write a function which does the opposite of `zip()`
+           }
 
-// Write tests here:
+           // Write tests here:
 
-assert(1 < 2)
-assert(1 + 2 == 3)
-assert([2, 3][0] === 2)
-// assert(reverse("3df") === "fd3")
+           assert(1 < 2)
+           assert(1 + 2 == 3)
+           assert([2, 3][0] === 2)
+               // assert(reverse("3df") === "fd3")
+               */
