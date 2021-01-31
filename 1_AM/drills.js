1bc1c4ae84cc4e742ad098389fbe1fab01a80996
mhowell11 <meganghowell@gmail.com>
2021-01-30 21:11:08 -0700

    attempted hw for day 1

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -4,7 +4,7 @@ const assert = require("assert");
 
 const reverse = function(str) {
     // TODO - write a function which reverses the string
-    let splitString = string1.split("");
+    let splitString = str.split("");
     let reverseArray = splitString.reverse();
     let joinArray = reverseArray.join("");
     return console.log(joinArray);
@@ -21,8 +21,7 @@ let factorial = function(num) {
         ans = ans * num;
         num -= 1;
     }
-    factorial = ans;
-    return console.log(factorial);
+    console.log(ans);
 }
 
 factorial(5);
@@ -41,38 +40,70 @@ const announceDate = function() {
 
 announceDate();
 
-const shiftRight = function(str, num) {
-        // TODO - write a function which shifts a string `num` characters to the right
-        // Example = shiftThree("Hello") === "lloHe"}
-        let letters = str.split("");
 
-        for (let i = 0; i < num; i++) {
-            //letters.unshiftletters.pop();
+
+const shiftRight = function(str, num) {
+    // TODO - write a function which shifts a string `num` characters to the right
+    // Example = shiftThree("Hello") === "lloHe"}
+    let newArr = [];
+    for (let i = 0; i < num; i++) {
+        newArr = arr.pop;
+    }
+    console.log(newArr);
+
+
+}
+
+shiftRight("Hello", 3);
+
+
+
+const tokenize = function(str) {
+    // TODO - write a function which converts a multi-word string into an array of words
+    let arr = [];
+    arr.push(str);
+    const arrLength = arr.length;
+    for (let i = 0; i < arrLength; i++) {
+        let holder = arr.find(" ");
+        arr.slice(holder);
+        console.log(arr);
+    }
+}
+
+tokenize("Hello World");
+
+const uniqueOnes = function(arr) {
+    // TODO - write a function which returns the inputted array without duplicate elements
+    const num = arr.length();
+    for (let i = 0; i < num + 1; i++) {
+        let eq = arr.sort();
+        if (eq = 0) {
+            arr.pop(i);
         }
     }
-    /*
-           const tokenize = function(str) {
-               // TODO - write a function which converts a multi-word string into an array of words
-           }
+    console.log(arr);
+}
 
-           const uniqueOnes = function(ary) {
-               // TODO - write a function which returns the inputted array without duplicate elements
-           }
+uniqueOnes([1, 2, 3, 4, 1, 5, 6, 2, 7]);
 
-           const zip = function(arr1, arr2) {
-               // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
-               // if the two arrays are of unequal length
-               // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
-           }
+const zip = function(arr1, arr2) {
+    // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
+    // if the two arrays are of unequal length
+    // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
+    console.log(arr1.sort(arr2));
+    arr1.join(arr2);
+}
 
-           const unzip = function(arr) {
-               // TODO - Write a function which does the opposite of `zip()`
-           }
+const unzip = function(arr) {
+    // TODO - Write a function which does the opposite of `zip()`
+    let arr1 = [];
+    let arr2 = [];
+    arr.slice((arr.length / 2));
+}
 
-           // Write tests here:
+// Write tests here:
 
-           assert(1 < 2)
-           assert(1 + 2 == 3)
-           assert([2, 3][0] === 2)
-               // assert(reverse("3df") === "fd3")
-               */
+//assert(1 < 2)
+//assert(1 + 2 == 3)
+//assert([2, 3][0] === 2)
+//assert(reverse("3df") === "fd3")
